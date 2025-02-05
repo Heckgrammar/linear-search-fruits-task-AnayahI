2759a35ce4d4a0b9235c7b138864056fc7ea33ed
Anayah <144434160+AnayahI@users.noreply.github.com>
2025-02-05 19:19:13 +0000

    Linear Search Homework

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -3,16 +3,20 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {// Linear Search Homework
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
-
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
+            Console.WriteLine("What word would you like to find?");
+            string value = Console.ReadLine();
+            bool found = false;
+            for (int i = 0; i < fruits.Length; i++) 
+            {
+                if (value == fruits[i])
+                {
+                    found = true;
+                }
+            }
+            if (found == true) { Console.WriteLine("True"); }
+            else { Console.WriteLine("False"); }
         }
     }
 }
